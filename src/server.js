e479f7fa2f8d0fe2de838a2b40edc398c7e29709
Jan Słomian <contact@jslomian.dev>
2024-02-26 19:12:58 +0100

    http

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ import path from 'path'
 import * as url from 'url';
 import {checkUser, requireAuth} from "../middleware/authMiddleware.js";
 import {db, dbGet} from "./database.js";
+import * as http from "http";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
@@ -21,11 +22,11 @@ let activeStreams = {}
 const nms = new NodeMediaServer(nmsConfig)
 nms.run()
 const app = express()
-const server = https.createServer({
-    key: fs.readFileSync(path.join(__dirname, '../', 'cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, '../', 'cert', 'cert.pem'))
-}, app)
-// const httpserver = http.createServer(app)
+// const server = https.createServer({
+//     key: fs.readFileSync(path.join(__dirname, '../', 'cert', 'key.pem')),
+//     cert: fs.readFileSync(path.join(__dirname, '../', 'cert', 'cert.pem'))
+// }, app)
+const server = http.createServer(app)
 const io = new Server(server)
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
