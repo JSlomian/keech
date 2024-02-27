321c5121d7cbb3d6b54cb512e272ce792345878d
Jan Słomian <contact@jslomian.dev>
2024-02-27 17:01:58 +0100

    ecosystem pm2 change

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,11 +4,5 @@ module.exports = {
         script: "npm",
         args: "run start",
         watch: true, // Optional: Watch for file changes and restart
-        env: {
-            NODE_ENV: "development",
-        },
-        env_production: {
-            NODE_ENV: "production",
-        }
     }]
 };
